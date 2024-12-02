2eec352851d4bf666d7abb45648e4fdc919850cc
badalchoudhary1 <badalror10@gmail.com>
2024-12-02 23:09:02 +0530

    delete some file

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn reciepe_app.wsgi
--- reciepe_app/settings.py
+++ reciepe_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bl-o_#t&uvh%0$*1lz!@k!8%-a@5_2^cm@+jbk_3!7b*h7a%og'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["vege.onrender.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'reciepe_app.urls'
@@ -118,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles' 
+# STATIC_ROOT = BASE_DIR / 'staticfiles' 
 # STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 # STATICFILES_DIR={
 #     os.path.join(BASE_DIR,"public/static")
