3c90d1a665019c45aea757e24be32a5d7fe2d5e9
badalchoudhary1 <badalror10@gmail.com>
2024-12-31 00:20:07 +0530

    create venv

--- reciepe_app/urls.py
+++ reciepe_app/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('vege/',include('vege.urls')),
+    path('',include('vege.urls')),
 ]
 
 # if settings.DEBUG:
--- vege/urls.py
+++ vege/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('update/<int:id>/', views.update_receipe, name="update_receipe"), 
     path('add_recipe/', views.add_recipe, name="add_recipe"), 
      path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
+    path('', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
